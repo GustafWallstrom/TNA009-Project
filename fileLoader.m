69d127e797726d8f7976e53e729a219e3b4d3673
Petra <gunnarsdotter@live.com>
2019-11-27 16:37:13 +0100

    change reading data

--- fileLoader.m
+++ fileLoader.m
@@ -1,12 +1,15 @@
 function [imageTensor] = fileLoader()
-   
-    imDir = '.\yalefaces\yalefaces\';
-    S = dir(fullfile(imDir,'*.pgm'));
-    imageTensor = zeros(243*320 ,11 , numel(S)/11);
+
+    imDir = '.\orl_faces\s'; %'.\yalefaces\yalefaces\';
+    imgSize = 92*112;
+    expression = 10;
+    people = 40;
+    imageTensor = zeros( imgSize , expression ,people);
     
-    for k = 1:numel(S)/11
-        for j = 1:11
-            F = fullfile(imDir,S(k).name);
+    for k = 1:people
+         S = dir(fullfile(strcat(imDir, num2str(k)),'*.pgm'));
+        for j = 1:expression
+            F = fullfile(strcat(imDir, num2str(k)),S(j).name);
             I = imread(F);
             I = I(:);
             imageTensor(:,j,k) = im2double(I);
