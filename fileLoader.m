634eef398ee3dca0dd4396b5ddcabc7906dfdd8e
GustafWallstrom <46966247+GustafWallstrom@users.noreply.github.com>
2019-11-27 14:52:59 +0100

    Updated file loader

--- fileLoader.m
+++ fileLoader.m
@@ -2,12 +2,15 @@ function [imageTensor] = fileLoader()
    
     imDir = '.\yalefaces\yalefaces\';
     S = dir(fullfile(imDir,'*.pgm'));
-    imageTensor = zeros(243, 320, numel(S));
+    imageTensor = zeros(243*320 ,11 , numel(S)/11);
     
-    for k = 1:numel(S)
-        F = fullfile(imDir,S(k).name);
-        I = imread(F);
-        imageTensor(:,:,k) = im2double(I);
+    for k = 1:numel(S)/11
+        for j = 1:11
+            F = fullfile(imDir,S(k).name);
+            I = imread(F);
+            I = I(:);
+            imageTensor(:,j,k) = im2double(I);
+        end
     end
 end
 
