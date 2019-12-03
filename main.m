9cdd184d7dc340008311dba3eb3d24f0a13632f5
GustafWallstrom <46966247+GustafWallstrom@users.noreply.github.com>
2019-12-03 16:06:55 +0100

    New database for faces

--- main.m
+++ main.m
@@ -1,18 +1,20 @@
 
- load('imgTensor.mat')
- load('tens.mat')
- z = imageTensor(:,5,16); %orl
- 
- subplot(1,3,1);
- imshow(reshape(z,[112, 92]))
+load('yface10.mat');
+X = imcomplement(yface10/max(max(max(yface10))));
 
-tol = 0.1;
-%[S, F, G, H, D, C] = createTensor(imageTensor, tol);
+tol = 0.001;
+e = 1;
+p = 2;
+
+z = X(:,e,p);
+subplot(1,3,1);
+imshow(reshape(z,[112, 78]))
+%[S, F, G, H, D, C] = createTensor(X, tol);
 
 % Least square
 [e1, p1] = classification(C, H, z, tol);
 
-im = reshape(imageTensor(:,e1,p1),[112,92]);
+im = reshape(X(:,e1,p1),[112,78]);
 subplot(1,3,2)
 imshow(im)
 
@@ -22,5 +24,21 @@ Be = ttm(S,G,2);
 [e2, p2] = classificationQR(Be, F, H, z, tol);
 
 subplot(1,3,3);
-imshow(reshape(imageTensor(:,e2,p2),[112,92]));
+imshow(reshape(X(:,e2,p2),[112,78]));
 
+%%
+clc
+tol = 0.001;
+for e = 1:11
+    for p = 1:10
+       z = X(:,e,p);
+       [e1, p1] =  classificationQR(Be, F, H, z, tol);
+       if(e1 ~= e || p ~= p1)
+           fprintf('p: %d ', p);
+           fprintf('p1: %d ', p1);
+           fprintf('e: %d ', e);
+           fprintf('e1: %d\n', e1);
+       end
+    end
+end
+fprintf('Done \n');
